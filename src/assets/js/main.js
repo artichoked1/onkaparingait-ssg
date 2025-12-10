02058cbbf0c96b6e0a4c1cfff3675afbfb9ac125
MozzaMozzarella <fellowdig36@gmail.com>
2025-12-10 23:22:51 +1030

    Fixed member card responsiveness. Added scroll effect and location section with interactive map

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,3 +12,35 @@ document.addEventListener('DOMContentLoaded', () => {
     updateNavBorder();
     window.addEventListener('scroll', updateNavBorder, { passive: true });
 });
+
+function scrollToElement(id, offset = 42) {
+    const element = document.getElementById(id);
+    const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+}
+
+// Initialise map
+const map = L.map('map', {
+    zoomControl: false
+}).setView([-35.22708204604496, 138.54444632445], 11);
+
+L.circle([-35.22180139887642, 138.52425147667432], {radius: 10000, weight: 1, fillColor: '#7DBEFF'}).addTo(map);
+L.circle([-35.18147515901024, 138.50284583247344], {radius: 10000, weight: 1, fillColor: '#7DBEFF'}).addTo(map);
+L.tileLayer(
+    'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+    {
+        attribution: '&copy; OpenStreetMap &copy; CARTO'
+    }
+).addTo(map);
+
+// L.tileLayer(
+//     'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+//     {
+//         attribution: '&copy; OpenStreetMap &copy; Stadia Maps'
+//     }
+// ).addTo(map);
